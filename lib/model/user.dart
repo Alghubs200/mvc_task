a8143198f327e0f0761415e15c5e46704f8e0a24
Alghubs200 <alghubs@outlook.com>
2022-10-21 16:13:19 +0530

    auth

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,26 @@
-// class User {
-//   String name;
-// }
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  String name;
+  String profilePhoto;
+  String uid;
+  String email;
+
+  User(
+      {required this.name,
+      required this.profilePhoto,
+      required this.uid,
+      required this.email});
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'profilePic': profilePhoto, 'uid': uid, 'email': email};
+
+  static User formSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+    return User(
+      email: snapshot['email'],
+      profilePhoto: snapshot['profilePhoto'],
+      uid: snapshot['uid'],
+      name: snapshot['snapshot'],
+    );
+  }
+}
